7b1f14c23566ce9b99c56825662ae59a4af91ee7
ivandjoh <ivandi.djoh@gmail.com>
2023-02-02 00:05:39 +0800

    main updated!

--- main.go
+++ main.go
@@ -39,5 +39,21 @@ func main() {
 	if err != nil {
 		log.Fatal("Error Om!", err)
 	}
+	// it became compact binary wire format
+	/**
+	 *
+	 * [18 39 8 1 18 12 67 101 108 97 110 97 32 68 97 108 97 109 25 0 0 0 0 0 106 232 64 32 2 42 10 8 1 18 6 99 101 108 97 110 97 18 40 8 2 18 13 67 101 108 97 110 97 32 80 101 110 100 101 107 25 0 0 0 0 128 79 2 65 32 22 42 10 8 1 18 6 99 101 108 97 110 97]
+	 *
+	 */
 	fmt.Println(data)
+
+	fmt.Println("-------- UnMarshal --------")
+
+	// decode using UnMarshal
+	hasilProduct := &pb.Products{}
+	if err = proto.Unmarshal(data, hasilProduct); err != nil {
+		log.Fatal("Unsuccess!", err)
+	}
+
+	fmt.Println(hasilProduct)
 }
