119d97b9453de085cb5c8c1894545942b6be791d
ivandjoh <ivandi.djoh@gmail.com>
2023-02-01 23:26:24 +0800

    proto updated

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	pb "go-grpc/pb"
+	"log"
+
+	"google.golang.org/protobuf/proto"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	products := &pb.Products{
+		Data: []*pb.Product{
+			{
+				Id:    1,
+				Name:  "Celana Dalam",
+				Price: 50000.00,
+				Stock: 2,
+				Category: &pb.Category{
+					Id:   1,
+					Name: "celana",
+				},
+			},
+			{
+				Id:    2,
+				Name:  "Celana Pendek",
+				Price: 150000.00,
+				Stock: 22,
+				Category: &pb.Category{
+					Id:   1,
+					Name: "celana",
+				},
+			},
+		},
+	}
+
+	data, err := proto.Marshal(products)
+	if err != nil {
+		log.Fatal("Error Om!", err)
+	}
+	fmt.Println(data)
 }
